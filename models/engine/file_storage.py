1601247f6e3617efc3287cbd3f722d816dd264a8
Amine <mouhamedamine.akchouche@gmail.com>
2024-03-09 08:20:29 -0800

    Json Ser/Dese

---
+++ models/engine/file_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+this file represents the Ser of instance to json file and vice versa
+
+"""
+
+class FileStorage():
+
+
+    __file_path=""
+    __objects= {}
+
+    def all(self):
+
+        return FileStorage.__objects
+    def new(self, obj):
+    
+        key= "{}.{}".format(type(obj).__name__.obj.id)
+        FileStorage.__objects[key]= obj
+
+    def save(self):
+
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(
+                {k: v.to_dict() for x,v in FileStorage.__objects.items()}, f)
+    def reload(self):
+        with open(FileStorage.__file_path, 'r') as f:
+            deserialized = None
+
+            try:
+                deserialized = json.load(f)
+            except json.JSONDecodeError:
+                pass
+
+            if deserialized is None:
+                return
